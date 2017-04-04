839b28dfe24a660bb2bf2bb6b3644761f8517573
joehua87 <joehua87@gmail.com>
2017-04-04 13:46:27 +0700

    refine deps + index.js, prepare to publish

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// @flow
+
+import createGetOne from './helpers/createGetOne'
+import createQueryExtractor from './helpers/createQueryExtractor'
+import createQueryResolver from './helpers/createQueryResolver'
+
+export {
+  createGetOne,
+  createQueryExtractor,
+  createQueryResolver,
+}
