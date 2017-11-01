ba52bfd16ab33625c8a529007a9300daa53bfc9e
joehua87 <joehua87@gmail.com>
2017-11-01 19:32:35 +0700

    get projection for fragment case

--- src/helpers/getProjection.js
+++ src/helpers/getProjection.js
@@ -9,7 +9,26 @@ export default function getProjection({
   field: string,
   info: any,
 }): string[] {
-  const fieldSelections = info.fieldNodes[0].selectionSet.selections
-  const { selectionSet: { selections } } = fieldSelections.find(x => x.name.value === field)
-  return R.pipe(R.map(x => x.name.value), R.filter(x => x !== '__typename'))(selections)
+  // console.log(JSON.stringify(info, null, 2))
+  const {
+    selectionSet: { selections },
+  } = info.fieldNodes[0].selectionSet.selections.find(x => x.name.value === field)
+  const fieldSelection = R.pipe(
+    R.filter(x => x.kind === 'Field'),
+    R.map(x => x.name.value),
+    R.filter(x => x !== '__typename'),
+  )(selections)
+  const fragmentSelection = R.pipe(
+    R.filter(x => x.kind === 'FragmentSpread'),
+    R.map(x => x.name.value),
+    R.filter(x => x !== '__typename'),
+  )(selections)
+
+  const fragmentFields = R.pipe(
+    R.map(name => info.fragments[name]),
+    R.map(x =>
+      x.selectionSet.selections.map(selection => selection.name.value)),
+    R.flatten,
+  )(fragmentSelection)
+  return [...fieldSelection, ...fragmentFields]
 }
